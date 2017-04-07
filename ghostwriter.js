f3fc2bbc6d6fa18b4949e475e0acc8fac6aac527
Unknown <khinrichs1@mail.csuchico.edu>
2017-04-06 20:01:24 -0700

    completed.

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,20 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+
+  var intro = ["What is this?", "I am", "What are you wearing, Jake from State farm?",
+  "On Wednesday's, we wear"];
+  var body = ["a school for ants?", "Batman", "khakis", "pink", "dude"];
+  var end = ["crying", "stoked"];
+
+  randIndex = randomUpTo(intro.length-1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length-1);
+  sentence = sentence + " " + body[randIndex];
+  randIndex = randomUpTo(body.length-1);
+  sentence = sentence + " " + end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
